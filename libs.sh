869300f3ea4edfebccba3cfc641a7633b739eded
Hien Nguyen <mcjambi@gmail.com>
2022-07-29 11:10:58 +0700

    update
    update

--- libs.sh
+++ libs.sh
@@ -2,7 +2,7 @@
 set -e
 
 VERSION="v2.9.0"
-URL="https://github.com/mcjambi/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
+URL="https://github.com/mcjambi/react-native-pjsip-builder/archive/refs/tags/${VERSION}.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
 DOWNLOAD=true
