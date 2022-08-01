c8599934f34918ba86092928e11a325b551fd461
Hien Nguyen <mcjambi@gmail.com>
2022-08-01 17:02:32 +0700

    update
    update

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,11 @@
-declare module "react-native-xsip"
+// declare module "react-native-xsip"
+
+/**
+ * Jamviet.com fixed typescript
+ */
+
+ export * as Account  from './src/Account';
+ export * as Call  from './src/Call';
+ export * as Endpoint  from './src/Endpoint';
+ export * as PreviewVideoView  from './src/PreviewVideoView';
+ export * as RemoteVideoView  from './src/RemoteVideoView';
