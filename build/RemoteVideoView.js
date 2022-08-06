e9212267ec275ee04085738b94a2143356ae4287
Hien Nguyen <mcjambi@gmail.com>
2022-08-07 00:00:31 +0700

    update: auto build while installing
    update: auto build while installing

--- build/RemoteVideoView.js
+++ build/RemoteVideoView.js
@@ -1,11 +1,11 @@
 import { requireNativeComponent } from 'react-native';
 import PropTypes from 'prop-types';
 const RemoteVideoView = {
-  name: 'PjSipRemoteVideoView',
-  propTypes: {
-    windowId: PropTypes.string.isRequired,
-    objectFit: PropTypes.oneOf(['contain', 'cover'])
-  }
+    name: 'PjSipRemoteVideoView',
+    propTypes: {
+        windowId: PropTypes.string.isRequired,
+        objectFit: PropTypes.oneOf(['contain', 'cover'])
+    },
 };
 const View = requireNativeComponent('PjSipRemoteVideoView');
 export default View;
---
+++ build/index.js
@@ -0,0 +1,19 @@
+import { default as Account } from './Account';
+import { default as Call } from './Call';
+import { default as Endpoint } from './Endpoint';
+import { default as PreviewVideoView } from './PreviewVideoView';
+import { default as RemoteVideoView } from './RemoteVideoView';
+export * from './Account';
+export * from './Call';
+export * from './Endpoint';
+export * from './PreviewVideoView';
+export * from './RemoteVideoView';
+export { Account, Call, Endpoint, };
+export default {
+    Account,
+    Call,
+    Endpoint,
+    PreviewVideoView,
+    RemoteVideoView,
+};
+//# sourceMappingURL=index.js.map
